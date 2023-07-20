feeb2eb1a97edd9b35ba42ae3f81278cd3b16017
CedarHuang <cedar.hxd@gmail.com>
2023-07-21 01:46:14 +0800

    commit code

---
+++ src/app.py
@@ -0,0 +1,23 @@
+import web
+
+class skip:
+    def GET(self):
+        return
+
+class process:
+    def GET(self, config_name):
+        if not config_name:
+            config_name = 'default'
+        local = {'web': web, 'config_name': config_name}
+        with open('process.py') as f:
+            exec(f.read(), local)
+        return local['res_body']
+
+urls = (
+    '/favicon.ico', 'skip',
+    '/(.*)', 'process',
+)
+
+app = web.application(urls, globals())
+if __name__ == "__main__":
+    app.run() # port: 3658
---
+++ src/process.py
@@ -0,0 +1,149 @@
+import grequests
+import json
+import re
+import sys
+import tomllib
+
+sys.setrecursionlimit(4096)
+
+toml = {
+    'status': {
+        'closed': '',
+        'live': '',
+        'blocked': '',
+        'exception': '',
+    },
+    're': {
+        'name': [],
+        'group': [],
+    },
+    'include': {
+        'm3u': {
+            'remote': [],
+        }
+    }
+}
+with open('../config/%s.toml' % config_name, 'rb') as f:
+    toml.update(tomllib.load(f))
+
+r = grequests.map([grequests.get(url) for url in toml['include']['m3u']['remote']])
+for i in r:
+    for j in i.text.splitlines():
+        info = re.search(r'^https?://[^/]+/([^/]+)/([^/]+)$', j)
+        if info is not None:
+            toml['channel'].append({
+                'platform': info.group(1),
+                'roomid': info.group(2),
+            })
+
+CLOSED = toml['status']['closed']
+LIVE = toml['status']['live']
+BLOCKED = toml['status']['blocked']
+EXCEPTION = toml['status']['exception']
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1'
+}
+
+retry_deepth_max = 2
+retry_deepth = 0
+
+class channel:
+    def gen(info):
+        try:
+            simple = info['simple'].split(':')
+            info['platform'] = simple[0]
+            info['roomid'] = simple[1]
+        except:
+            pass
+        try:
+            return eval(info['platform'] + '(info)')
+        except:
+            return exception(info)
+    def __init__(self, info):
+        self.i = info
+    def proc_res(self, response):
+        global retry_deepth
+        try:
+            self.proc_res_impl(response)
+        except:
+            if retry_deepth < retry_deepth_max:
+                retry_deepth += 1
+                response = grequests.map([grequests.get(response.url, headers=headers)])[0]
+                channel.gen(self.__dict__['i']).proc_res(response)
+            else:
+                exception(self.__dict__['i']).proc_res_impl(response)
+        retry_deepth = 0
+    def gen_m3u_item(self, toml):
+        for key in toml['re']:
+            for re_item in toml['re'][key]:
+                try:
+                    self.i[key] = eval('re.sub(%s, self.i[key])' % re_item)
+                except:
+                    pass
+        self.i['name'] = toml['format']['name'].format(**self.i)
+        self.i['group'] = toml['format']['group'].format(**self.i)
+        for key in ['name', 'group']:
+            for re_item in toml['re'][key]:
+                try:
+                    self.i[key] = eval('re.sub(%s, self.i[key])' % re_item)
+                except:
+                    pass
+        res = '#EXTINF:-1 tvg-logo="%s" group-title="%s",%s\n' % (self.i['logo'], self.i['group'], self.i['name'])
+        res += toml['format']['url'].get(self.i['platform'], toml['format']['url']['default']).format(**self.i) + '\n'
+        return res
+
+class exception(channel):
+    def gen_req(self):
+        return grequests.get('')
+    def proc_res_impl(self, response):
+        self.i['nick'] = self.i['roomid']
+        self.i['title'] = self.i['platform'] + ':' + self.i['roomid']
+        self.i['area'] = self.i['platform']
+        self.i['logo'] = ''
+        self.i['status'] = EXCEPTION
+
+class douyu(channel):
+    def gen_req(self):
+        return grequests.get('https://m.douyu.com/' + self.i['roomid'], headers=headers)
+    def proc_res_impl(self, response):
+        info = json.loads(re.search(r'<script id="vite-plugin-ssr_pageContext" type="application/json">(.*)</script>', response.text).group(1))
+        info = info['pageContext']['pageProps']['room']['roomInfo']['roomInfo']
+        self.i['nick'] = info['nickname']
+        self.i['title'] = info['roomName']
+        self.i['area'] = info['cate2Name']
+        self.i['logo'] = info['avatar']
+        self.i['status'] = LIVE if info['isLive'] == 1 else CLOSED
+
+class huya(channel):
+    def gen_req(self):
+        return grequests.get('https://m.huya.com/' + self.i['roomid'], headers=headers)
+    def proc_res_impl(self, response):
+        info = json.loads(re.search(r'<script> window.HNF_GLOBAL_INIT = (.*) </script>', response.text).group(1))
+        info = info['roomInfo']['tLiveInfo'] if info['roomInfo']['eLiveStatus'] != 1 else info['roomInfo']['tRecentLive']
+        self.i['nick'] = info['sNick']
+        self.i['title'] = info['sIntroduction']
+        self.i['area'] = info['sGameFullName']
+        self.i['logo'] = info['sAvatar180']
+        self.i['status'] = LIVE if info['_classname'] == 'LiveRoom.LiveInfo' else CLOSED
+
+class bilibili(channel):
+    def gen_req(self):
+        return grequests.get('https://live.bilibili.com/h5/' + self.i['roomid'], headers=headers)
+    def proc_res_impl(self, response):
+        info = json.loads(re.search(r'<script>window.__NEPTUNE_IS_MY_WAIFU__=(.*?)</script>', response.text).group(1))
+        init_info = info['roomInitRes']['data']
+        info = info['roomInfoRes']['data']
+        self.i['nick'] = info['anchor_info']['base_info']['uname']
+        self.i['title'] = info['room_info']['title']
+        self.i['area'] = info['room_info']['area_name']
+        self.i['logo'] = info['anchor_info']['base_info']['face']
+        self.i['status'] = LIVE if info['room_info']['live_status'] == 1 else CLOSED
+        self.i['status'] = BLOCKED if init_info['is_locked'] == True else self.i['status']
+
+web.header('content-type', 'text/plain; charset=utf-8')
+res_body = '#EXTM3U\n'
+channels = [channel.gen(i) for i in toml['channel']]
+responses = grequests.map([i.gen_req() for i in channels])
+[channel.proc_res(response) for (channel, response) in list(zip(channels, responses))]
+res_body += ''.join([channel.gen_m3u_item(toml) for channel in channels])
