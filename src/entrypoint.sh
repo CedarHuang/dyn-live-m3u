61a790929f04bed5880651bde2ee0cf36ef16af7
CedarHuang <cedar.hxd@gmail.com>
2023-07-22 13:14:38 +0800

    优化extrypoint.sh, 在docker中自动生成配置文件

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,5 +3,14 @@ cd /app/dyn-live-m3u
 git pull
 pip install -r requirements.txt
 cp ./src/* ..
+FROM="./config"
+TO="/config"
+for file in "$FROM"/*; do
+    filename=$(basename "$file")
+    if [ ! -e "$TO/$filename" ]; then
+        cp "$file" "$TO"
+        echo "$filename does not exist, has been generated"
+    fi
+done
 cd /app
 python app.py 3658
