9f87d6b22389fb3149f7b7312f25cd76cb2b68be
CedarHuang <cedar.hxd@gmail.com>
2023-08-09 19:31:36 +0800

    update douyu

--- src/process.py
+++ src/process.py
@@ -113,7 +113,7 @@ class douyu(channel):
         return grequests.get('https://m.douyu.com/' + self.i['roomid'], headers=headers)
     def proc_res_impl(self, response):
         info = json.loads(re.search(r'<script id="vite-plugin-ssr_pageContext" type="application/json">(.*)</script>', response.text).group(1))
-        info = info['pageContext']['pageProps']['room']['roomInfo']['roomInfo']
+        info = info['pageProps']['room']['roomInfo']['roomInfo']
         self.i['nick'] = info['nickname']
         self.i['title'] = info['roomName']
         self.i['area'] = info['cate2Name']
